8ac4931fa803a13bbc611d2f0e48450e3d5e0f0c
Yifei Guo <yifeiguo918@gmail.com>
2018-01-14 14:01:12 -0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Yifei Guo on 1/14/18.
+//  Copyright © 2018 Yifei Guo. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
